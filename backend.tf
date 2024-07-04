de84563c4f52b74726b31f13dc2e0fd0ac34cc13
Wong Tsang Han <tsanghan@gmail.com>
2024-07-05 00:58:50 +0800

    2nd commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce6-tfstate"
-    key    = "tsanghan-ce6-mod2_10_with_route53.tfstate"
+    key    = "tsanghan-ce6-mod2_10-with-route53.tfstate"
     region = "ap-southeast-1"
   }
 }
